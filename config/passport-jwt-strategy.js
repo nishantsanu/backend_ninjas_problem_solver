aeaae19b331641e87122d159f645e6affb5a2c90
NISHANT KUMAR <nkumarnet40@gmail.com>
2021-08-09 22:55:21 +0530

    passport

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -14,50 +14,66 @@ let opts = {
 }
 // 
 //telling passport to use jwt strategy
-passport.use(new JWTStrategy(opts, function (jwtPayLoad, done) {
+passport.use(new JWTStrategy(opts, async function (jwtPayLoad, done) {
     var found=false;
     let Db = Student;
-    Student.findById(jwtPayLoad._id, function (err, user) {
-        if (err) {
-            return done(err,false);
-        }
 
-        //if user is found
-        else if (user) {
-            found=true;
+    let student= await Student.findById(jwtPayLoad._id);
+    if(user){
+        return done(null,student);
+    }
+    let ta= await Ta.findById(jwtPayLoad._id);
+    if(ta){
+        return done(null,ta);
+    }
+    let teacher= await Teacher.findById(jwtPayLoad._id);
+    if(teacher){
+        return done(null,teacher);
+    }
 
-            return done(null, user);
-        }
+    return done(true,false);
 
-    });
-    // Db=Ta;
-    Ta.findById(jwtPayLoad._id, function (err, user) {
-        if (err) {
-            return done(err,false);
-        }
-        //if user is found
-        else if (user) {
-            found=true;
-            return done(null, user);
+    //  Student.findById(jwtPayLoad._id, function (err, user) {
+    //     if (err) {
+    //         return done(err,false);
+    //     }
 
-        }
-    });
-    // Db=Teacher;
-    Teacher.findById(jwtPayLoad._id, function (err, user) {
+    //     //if user is found
+    //     else if (user) {
+    //         found=true;
 
-        if (err) {
+    //         return done(null, user);
+    //     }
+
+    // });
+    // // Db=Ta;
+    // Ta.findById(jwtPayLoad._id, function (err, user) {
+    //     if (err) {
+    //         return done(err,false);
+    //     }
+    //     //if user is found
+    //     else if (user) {
+    //         found=true;
+    //         return done(null, user);
+
+    //     }
+    // });
+    // // Db=Teacher;
+    // Teacher.findById(jwtPayLoad._id, function (err, user) {
+
+    //     if (err) {
             
-            return done(err,false);
-        }
-        //if user is found
-        else if (user) {
-            found=true;
-            return done(null, user);
-        }
-        else if(found===false){
-            return done(err,false);
-        }
-    });
+    //         return done(err,false);
+    //     }
+    //     //if user is found
+    //     else if (user) {
+    //         found=true;
+    //         return done(null, user);
+    //     }
+    //     else if(found===false){
+    //         return done(err,false);
+    //     }
+    // });
 }));
 
 module.exports = passport;
